83059e705341b54e64e7aea1cbc2f860211c4177
Tomas Poveda <tpovedatd@gmail.com>
2020-08-22 06:44:24 +0200

    Cleanup plugin loader

--- artellapipe/launcher/plugins/dccselector/loader.py
+++ artellapipe/launcher/plugins/dccselector/loader.py
@@ -14,19 +14,17 @@ __email__ = "tpovedatd@gmail.com"
 
 import os
 import sys
-import inspect
 import logging.config
 
 import tpDcc as tp
 
 
-def init(do_reload=False, dev=False):
+def init(dev=False):
     """
     Initializes module
     :param do_reload: bool, Whether to reload modules or not
     """
 
-    from tpDcc.libs.python import importer
     from artellapipe.launcher.plugins.dccselector import register
 
     logger = create_logger()
@@ -39,38 +37,7 @@ def init(do_reload=False, dev=False):
         except RuntimeError:
             sentry_sdk.init("https://ce5e3ff0ce7744b38ba65ff9067d658b@sentry.io/1796914", default_integrations=False)
 
-    class DCCSelectorPlugin(importer.Importer, object):
-        def __init__(self, debug=False):
-            super(DCCSelectorPlugin, self).__init__(module_name='artellapipe.launcher.plugins.dccselector', debug=debug)
-
-        def get_module_path(self):
-            """
-            Returns path where tpNameIt module is stored
-            :return: str
-            """
-
-            try:
-                mod_dir = os.path.dirname(inspect.getframeinfo(inspect.currentframe()).filename)
-            except Exception:
-                try:
-                    mod_dir = os.path.dirname(__file__)
-                except Exception:
-                    try:
-                        import artellapipe.launcher.plugins.artellamanager
-                        mod_dir = artellapipe.launcher.plugins.artellamanager.__path__[0]
-                    except Exception:
-                        return None
-
-            return mod_dir
-
-    packages_order = []
-
     update_paths()
-    launcher_importer = importer.init_importer(importer_class=DCCSelectorPlugin, do_reload=False, debug=dev)
-    launcher_importer.import_packages(order=packages_order, only_packages=False)
-    if do_reload:
-        launcher_importer.reload_all()
-
     register_resources()
 
 
